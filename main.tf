e462ff6d264e965cd7846c2c5b9b309abf11bb87
Srishti Jayshyam Singh <srishti.singh@sigmoidanalytics.com>
2025-10-29 16:48:25 +0530

    commit

--- main.tf
+++ main.tf
@@ -1,20 +1,8 @@
-###########################################################
-# PROVIDER & RESOURCE GROUP
-###########################################################
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
     }
   }
 }
@@ -23,195 +11,168 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "main" {
+# Resource Group
+resource "azurerm_resource_group" "rg" {
   name     = "tf-azure-rg"
-  location = "East US"
+  location = "centralindia"
 }
 
-###########################################################
-# NETWORKING
-###########################################################
-
-resource "azurerm_virtual_network" "main" {
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
   name                = "tf-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
+# Subnets
 resource "azurerm_subnet" "public" {
   name                 = "public-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_subnet" "private" {
   name                 = "private-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-###########################################################
-# NETWORK SECURITY GROUP (allow SSH + HTTP)
-###########################################################
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "tf-nsg"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  security_rule {
-    name                       = "Allow-SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-HTTP"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-###########################################################
-# PUBLIC IP + NETWORK INTERFACES
-###########################################################
-
+# Public IP
 resource "azurerm_public_ip" "public_ip" {
   name                = "tf-public-ip"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
 
-resource "azurerm_network_interface" "public_nic" {
-  name                = "public-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+# Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "tf-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "80"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "22"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
+}
+
+# NIC for Public VM
+resource "azurerm_network_interface" "nic_public" {
+  name                = "nic-public"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "public-ip-config"
+    name                          = "public-nic-ip"
     subnet_id                     = azurerm_subnet.public.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
-resource "azurerm_network_interface" "private_nic" {
-  name                = "private-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+# NIC for Private VM
+resource "azurerm_network_interface" "nic_private" {
+  name                = "nic-private"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "private-ip-config"
+    name                          = "private-nic-ip"
     subnet_id                     = azurerm_subnet.private.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-###########################################################
-# SSH KEY
-###########################################################
-
-resource "tls_private_key" "example" {
+# SSH Key
+resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "local_file" "private_key_pem" {
-  content  = tls_private_key.example.private_key_pem
+resource "local_file" "private_key" {
+  content  = tls_private_key.ssh_key.private_key_pem
   filename = "${path.module}/id_rsa"
 }
 
-###########################################################
-# PUBLIC VM (NGINX)
-###########################################################
-
+# Public VM with NGINX
 resource "azurerm_linux_virtual_machine" "public_vm" {
   name                = "public-vm"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic_public.id]
   size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  network_interface_ids = [azurerm_network_interface.public_nic.id]
+
+  admin_username = "azureuser"
   disable_password_authentication = true
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = tls_private_key.example.public_key_openssh
+    public_key = tls_private_key.ssh_key.public_key_openssh
   }
 
   os_disk {
-    name                 = "public-os-disk"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 
   custom_data = filebase64("${path.module}/nginx-cloudinit.yml")
 }
 
-###########################################################
-# PRIVATE VM
-###########################################################
-
+# Private VM
 resource "azurerm_linux_virtual_machine" "private_vm" {
   name                = "private-vm"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic_private.id]
   size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  network_interface_ids = [azurerm_network_interface.private_nic.id]
+
+  admin_username = "azureuser"
   disable_password_authentication = true
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = tls_private_key.example.public_key_openssh
+    public_key = tls_private_key.ssh_key.public_key_openssh
   }
 
   os_disk {
-    name                 = "private-os-disk"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
-}
-
-###########################################################
-# OUTPUTS
-###########################################################
-
-output "public_ip" {
-  description = "Public IP of the NGINX server"
-  value       = azurerm_public_ip.public_ip.ip_address
-}
-
-output "private_vm_name" {
-  value = azurerm_linux_virtual_machine.private_vm.name
 }
